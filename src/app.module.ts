5131b1204fb006b7e5ee61d7993bb32dfedc3dc7
heart <7362469@qq.com>
2023-07-19 01:03:53 +0800

    feat: add role module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, RoleModule],
   controllers: [AppController],
   providers: [AppService],
 })
